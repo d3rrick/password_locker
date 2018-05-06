8dfea3f7782c2c1e1229a2f696e52e6c8a01cb52
muriithiderro <muriithiderrick56@gmail.com>
2018-05-06 14:53:51 +0300

    added search for credentials method

--- test_credential.py
+++ test_credential.py
@@ -2,18 +2,35 @@ import unittest
 from credential import Credential
 
 class TestCredential(unittest.TestCase):
+	def setUp(self):
+		"""this method runs before each test"""
+		self.c= Credential("github", "muriithiderro","muriithiderro@gmail.com","hhrh4ui4jr")
+		self.newlist = []
+		self.newlist.append(self.c)
+	def test_init(self):
+		"""this method checks whether instantiation is correctly done"""
 
-	@classmethod
-	def setUpCredentials(cls):
-		print("setupe begun")
+		self.assertEqual(self.c.account, 'github')
+		self.assertEqual(self.c.username, 'muriithiderro')
+		self.assertEqual(self.c.email, 'muriithiderro@gmail.com')
+		self.assertEqual(self.c.password, 'hhrh4ui4jr')
+	
+	def test_number_of_credential(self):
+		"""this methods searches for the number of credentials for a given user"""
+		print(self.newlist)
+		self.assertEqual(len(self.newlist),1)
+	def test_search_for_credentials(self):
+		"""this method checks whether the searched account matches to any account in the system"""
+		test_credential = Credential("github", "muriithiderro","muriithiderro@gmail.com","hhrh4ui4jr")
+		found_credential = Credential("github", "muriithiderro","muriithiderro@gmail.com","hhrh4ui4jr")
+		self.assertEqual(found_credential.account, "github")
 
-	def SetUp(self):
-		""" setup method rub before each testcase"""
-		self.credential1 = Credential("github", "muriithiderro","muriithiderro@gmail.com","hhrh4ui4jr")
-		self.credential2 =  Credential("facebook", "muriirro","muriithiderro@gmail.com","hhjeio9i")
 
-	def bbbb(self):
-		self.assertEqual(self.credential1.account, 'github')
+
+
+
+
+
 
 
 if __name__ == '__main__':
