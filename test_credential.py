ae4bf62aa7eac0220633a4e106ed0c08cd5ddd48
muriithiderro <muriithiderrick56@gmail.com>
2018-05-06 14:24:39 +0300

    added a setup method to the testclass

--- test_credential.py
+++ test_credential.py
@@ -1,1 +1,20 @@
-import unit
+import unittest
+from credential import Credential
+
+class TestCredential(unittest.TestCase):
+
+	@classmethod
+	def setUpCredentials(cls):
+		print("setupe begun")
+
+	def SetUp(self):
+		""" setup method rub before each testcase"""
+		self.credential1 = Credential("github", "muriithiderro","muriithiderro@gmail.com","hhrh4ui4jr")
+		self.credential2 =  Credential("facebook", "muriirro","muriithiderro@gmail.com","hhjeio9i")
+
+	def bbbb(self):
+		self.assertEqual(self.credential1.account, 'github')
+
+
+if __name__ == '__main__':
+	unittest.main()
